7306cee3875f521cddd49436ed78ef16653fb663
gihwan <ghbaek88@naver.com>
2020-05-28 19:48:17 +0900

    20년5월28일 커밋

---
+++ src/member/dao/MemberDAO.java
@@ -0,0 +1,127 @@
+package member.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import member.been.MemberDTO;
+
+public class MemberDAO {
+    private static MemberDAO instance; // 싱글톤이라는 티를 팍팍 낸다.
+    
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String userName = "c##java";
+	private String password = "bit";
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;		
+   
+    public MemberDAO() {
+    	// 드라이버로딩
+		try {
+			Class.forName(driver);
+			System.out.println("드라이버 로딩 성공!");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 	
+	} // 생성자
+    // 오라클 접속하기
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(url, userName, password);
+			System.out.println("오라클 접속 성공!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+	}// getConnection()
+
+    
+    public static MemberDAO getInstance() { 
+    	if(instance == null) {
+    		// 스레드 동기화 (MemberDAO.class로 아무리 많이 들어와도 한사람만 처리한다?? 락을 건다??) 
+    		synchronized(MemberDAO.class) {
+    			instance = new MemberDAO(); // 메모리에 한번밖에 생성이 안된다.
+			}
+    	}
+        return instance;
+    }// getInstance()
+
+	public int writeMember(MemberDTO memberDTO) {
+		int su = 0;
+		getConnection(); // 오라클 접속
+		String sql = "insert into member values(?,?,?,?,?,?,?,?,?,?,?,?,sysdate)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, memberDTO.getName());
+			pstmt.setString(2, memberDTO.getId());
+			pstmt.setString(3, memberDTO.getPwd());
+			pstmt.setString(4, memberDTO.getGender());
+			pstmt.setString(5, memberDTO.getEmail1());
+			pstmt.setString(6, memberDTO.getEmail2());
+			pstmt.setString(7, memberDTO.getTel1());
+			pstmt.setString(8, memberDTO.getTel2());
+			pstmt.setString(9, memberDTO.getTel3());
+			pstmt.setString(10, memberDTO.getZipcode());
+			pstmt.setString(11, memberDTO.getAddr1());
+			pstmt.setString(12, memberDTO.getAddr2());
+			
+			// 실행
+			su = pstmt.executeUpdate();	
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 끊을때는 반대로 끊어야 한다!
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+		return su;
+	}
+	public ArrayList<MemberDTO> selectAll() {
+		
+		ArrayList<MemberDTO> ar = new ArrayList<MemberDTO>();
+		getConnection();
+		String sql = "select * from member";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				MemberDTO memberDTO = new MemberDTO();
+				memberDTO.setName(rs.getString("name"));
+				memberDTO.setId(rs.getString("id"));
+				memberDTO.setPwd(rs.getString("pwd"));
+				
+				ar.add(memberDTO);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			ar = null;
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return ar;
+	}
+
+
+}// class MemberDAO
+
---
+++ src/member/service/LoginServlet.java
@@ -0,0 +1,61 @@
+package member.service;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.been.MemberDTO;
+import member.dao.MemberDAO;
+
+
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	int sw = 0;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		// post 방식일때만  한글처리
+		request.setCharacterEncoding("UTF-8");
+
+		// 데이터
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		// DB 
+		MemberDAO dao = new MemberDAO();
+		ArrayList<MemberDTO> ar = dao.selectAll();
+
+		
+		// 응답
+		response.setContentType("text/html;charset=UTF-8");
+    	PrintWriter out = response.getWriter();
+    	out.println("<html>");
+    	out.println("<head>");
+    	out.println("<title>회원가입</title>");
+	
+		for(int i=0; i<ar.size(); i++) {
+			if(ar.get(i).getId().equals(id) && ar.get(i).getPwd().equals(pwd)) {
+				out.println(ar.get(i).getName()+"님 로그인 성공");
+				sw = 1;
+			}				
+		}    		
+    	if(sw == 0) out.println("로그인실패");
+    	
+    	out.println("<body>");
+    	out.println("</body>");    	
+    	out.println("</head>");
+    	out.println("</html>");
+	}
+	
+	
+	
+}
